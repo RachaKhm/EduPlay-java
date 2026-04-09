10940943d3a99c6be7a0b3b427aa9daef11414f5
toumidhia <138765744+toumidhia@users.noreply.github.com>
2026-04-09 22:28:14 +0100

    create user CRUD

--- src/main/java/dev/eduplay/mains/Main.java
+++ src/main/java/dev/eduplay/mains/Main.java
@@ -6,6 +6,7 @@ import dev.eduplay.tools.MyDataBase;
 public class Main {
     public static void main(String[] args){
         UserService us = new UserService();
+        System.out.println(us);
 
     }
 }
